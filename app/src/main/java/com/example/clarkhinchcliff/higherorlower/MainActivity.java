8741a5c35f42cb309eabea1c4806f1a70a5d47c2
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-08 22:27:37 -0700

    UI done for random number guessing app

--- app/src/main/java/com/example/clarkhinchcliff/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/higherorlower/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.clarkhinchcliff.higherorlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickEvent(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
