39c0e08f35e6dd8c1431f93173b6ad6999a2cdb9
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-08 22:46:26 -0700

    Functionality complete

--- app/src/main/java/com/example/clarkhinchcliff/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/higherorlower/MainActivity.java
@@ -3,15 +3,39 @@ package com.example.clarkhinchcliff.higherorlower;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickEvent(View view) {
+    int randomNumber;
+    String message = "";
+
+    public void checkGuess(View view) {
+        EditText guessedNumber = (EditText) findViewById(R.id.numberTextField);
+        String guessedNumberString = guessedNumber.getText().toString();
+        int guessedNumberInt = Integer.parseInt(guessedNumberString);
+
+        if (guessedNumberInt == randomNumber) {
+            message = "Perfect, you got it!";
+        } else if (guessedNumberInt > randomNumber) {
+            message = "A little too high";
+        } else {
+            message = "Too low";
+        }
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
 
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Random randomGenerator = new Random();
+
+        randomNumber = randomGenerator.nextInt(21);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
